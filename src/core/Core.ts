1f32fec768153e2376015d201a52258b5846791b
MuTsunTsai <don.m.t.tsai@gmail.com>
2019-09-24 05:43:01 +0800

    Terminate on next commit

--- src/core/Core.ts
+++ src/core/Core.ts
@@ -2,7 +2,10 @@
 class Core {
 
 	/** 已接到通知、等待被執行的 Observer */
-	private static _queue: Set<Observer> = new Set();
+	private static readonly _queue: Set<Observer> = new Set();
+
+	/** 等待終結的物件 */
+	private static readonly _terminate: Set<ShrewdObject> = new Set();
 
 	/** 在當前堆疊中是否已經設置了自動認可 */
 	private static _promised: boolean = false;
@@ -11,14 +14,16 @@ class Core {
 		Global.$pushState({ $isCommitting: true });
 
 		// 開始執行認可
-		while(Core._queue.size > 0) {
-			for(let observer of Core._queue) Observer.$render(observer);
-		}
+		for(let observer of Core._queue) Observer.$render(observer);
 
 		// 結束認可
 		Observer.$clearPending();
 		Core._queue.clear();
 		Global.$restore();
+
+		// 終結物件
+		for(let shrewd of Core._terminate) shrewd.$terminate();
+		Core._terminate.clear();
 	}
 
 	/** 自動認可會在堆疊清空時立刻執行（比任何 setTimeout 都更早） */
@@ -56,6 +61,6 @@ class Core {
 	}
 
 	public static $terminate(target: object) {
-		if(HiddenProperty.$has(target, $shrewdObject)) target[$shrewdObject].$terminate();
+		if(HiddenProperty.$has(target, $shrewdObject)) Core._terminate.add(target[$shrewdObject]);
 	}
 }
