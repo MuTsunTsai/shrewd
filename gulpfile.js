2c3a4f70cfdffffe1ecdc9d3e51b433219b8b9bc
MuTsunTsai <don.m.t.tsai@gmail.com>
2019-09-24 10:14:35 +0800

    Add sourcemaps to test

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
 var ts = require('gulp-typescript');
 var rename = require('gulp-rename');
 var umd = require('gulp-umd');
 var terser = require('gulp-terser');
 var ifAnyNewer = require('gulp-if-any-newer');
+var sourcemaps = require('gulp-sourcemaps');
 var headerComment = require('gulp-header-comment');
 
 var terserOption = {
@@ -22,7 +22,6 @@ let project = ts.createProject('src/tsconfig.json');
 
 gulp.task('build', () =>
 	project.src()
-		.pipe(plumber())
 		.pipe(project())
 		.pipe(umd({ exports: () => "Shrewd" }))
 		.pipe(headerComment(`
@@ -41,8 +40,9 @@ var testProject = ts.createProject('test/tsconfig.json');
 gulp.task('buildTest', () =>
 	testProject.src()
 		.pipe(ifAnyNewer("test/tests"))
-		.pipe(plumber())
+		.pipe(sourcemaps.init())
 		.pipe(testProject())
+		.pipe(sourcemaps.write({includeContent: false, sourceRoot: '../src'}))
 		.pipe(gulp.dest('test/tests'))
 );
 
